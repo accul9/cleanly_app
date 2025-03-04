0951fb6e339aef92176fb7ab2bc36d67bcfb0c35
Elsie C <elsie.720@outlook.com>
2025-03-05 04:28:51 +0900

    Serializerにあるメソッドの位置を修正。Django Browsable APIでCRUDをテストして問題無かったようです。

--- backend_project/backend/cleanly_backend/apiv1/serializers.py
+++ backend_project/backend/cleanly_backend/apiv1/serializers.py
@@ -10,13 +10,13 @@ class TaskSerializer(serializers.ModelSerializer):
         model = Task
         fields = "__all__"
         extra_kwargs = {
-            "is_completed": {"required": False},
-            "default": False,
+            "is_completed": {"required": False, "default": False},
             "title": {"error_messages": {"required": "タイトルは必須です。"}},
         }
 
-        def is_completed_label(self, obj):
-            return "完了" if obj.is_completed else "未着手"
+    # メソッド名はget_フィールド名の形式で定義
+    def get_is_completed_label(self, obj):
+        return "完了" if obj.is_completed else "未着手"
 
-        def frequency_label(self, obj):
-            return obj.get_frequency_display()
+    def get_frequency_label(self, obj):
+        return obj.get_frequency_display()
